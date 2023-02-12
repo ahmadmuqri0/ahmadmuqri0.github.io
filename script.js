810c20bed9c0bce7228281dea05929fd282dc4e5
Artemis <106456832+ahmadmuqri0@users.noreply.github.com>
2023-02-12 17:53:26 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const submitButton = document.querySelector(".submit")
 const numRating = document.getElementById("number")
 const rates = document.querySelectorAll(".rating")
 
-submitButton.addEventListener("click", () => {
-    secondPanel.classList.remove("hidden")
-    mainPanel.style.display = "none"
-})
-
 rates.forEach((rate) => {
     rate.addEventListener("click", () => {
         numRating.innerHTML = rate.innerHTML
+        
+        submitButton.addEventListener("click", () => {
+            secondPanel.classList.remove("hidden")
+            mainPanel.style.display = "none"
+        })
     })
 })
