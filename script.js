aa4387f939ca77a042fd506c044160008f007b37
Artemis <106456832+ahmadmuqri0@users.noreply.github.com>
2023-02-13 12:58:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const mainPanel = document.querySelector(".panel")
 const secondPanel = document.querySelector(".second-panel")
-const submitButton = document.querySelector(".submit")
-const numRating = document.getElementById("number")
-const rates = document.querySelectorAll(".rating")
+const submitButton = document.querySelector(".btn-submit")
+const numRating = document.querySelector(".rating")
+const rates = document.querySelectorAll(".rate-btn")
 
 rates.forEach((rate) => {
     rate.addEventListener("click", () => {
